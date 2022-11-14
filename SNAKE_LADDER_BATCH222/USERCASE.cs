e3f42b64798cf5cc21e38af8bb967286cc773c7d
Aneesh <katta.aneesh597@gmail.com>
2022-11-14 23:17:24 +0530

    uc2

--- SNAKE_LADDER_BATCH222/USERCASE.cs
+++ SNAKE_LADDER_BATCH222/USERCASE.cs
@@ -11,8 +11,14 @@ namespace SNAKE_LADDER_BATCH222
         static void Main()
         {
             int START_POSITION = 0;
+            int END_POSITION = 100;
+            int dieRoll;
             Console.WriteLine("Welcome to snake and Ladder");
             Console.WriteLine("Player will Start at Zero Position,Player position :{0}", START_POSITION);
+            Random random = new Random();
+            dieRoll = random.Next(1, 7);
+            Console.WriteLine("Number on die is: " + dieRoll);
+            Console.WriteLine("Player position:{0}", dieRoll);
 
         }
     }
