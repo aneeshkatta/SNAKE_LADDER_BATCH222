96e4afee60d760fd0c223c4662fdb0c67877c863
Aneesh <katta.aneesh597@gmail.com>
2022-11-14 23:13:52 +0530

    welcome

---
+++ SNAKE_LADDER_BATCH222/USERCASE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNAKE_LADDER_BATCH222
+{
+    internal class USERCASE
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to snake and Ladder");
+
+        }
+    }
+}
