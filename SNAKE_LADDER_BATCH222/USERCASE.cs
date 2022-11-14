d64b00686c9852ce658a023644e29177a441eb07
Aneesh <katta.aneesh597@gmail.com>
2022-11-14 23:21:15 +0530

    added uc3

--- SNAKE_LADDER_BATCH222/USERCASE.cs
+++ SNAKE_LADDER_BATCH222/USERCASE.cs
@@ -11,15 +11,50 @@ namespace SNAKE_LADDER_BATCH222
         static void Main()
         {
             int START_POSITION = 0;
-            int END_POSITION = 100;
-            int dieRoll;
+             int WIN_POSITION = 100;
+            int CURRENTPOSITION = 0;
+            const int forward = 1;
+            const int snakebite = 2;
             Console.WriteLine("Welcome to snake and Ladder");
             Console.WriteLine("Player will Start at Zero Position,Player position :{0}", START_POSITION);
             Random random = new Random();
-            dieRoll = random.Next(1, 7);
-            Console.WriteLine("Number on die is: " + dieRoll);
-            Console.WriteLine("Player position:{0}", dieRoll);
+            Random random1 = new Random();
+            while (CURRENTPOSITION < WIN_POSITION)
+            {
+                int checkOptions = random1.Next(0, 3);
+                int dieRoll = random.Next(1, 7);
+                switch (checkOptions)
+                {
+                    case forward:
+                        {
+                            Console.WriteLine("Player will move forward");
+                            if (CURRENTPOSITION < 100)
+                            {
+                                CURRENTPOSITION += dieRoll;
+                            }
 
+                        }
+                        break;
+                    case snakebite:
+                        {
+                            Console.WriteLine("Player is bitten by a snake");
+                            if (CURRENTPOSITION > 0)
+                            {
+                                CURRENTPOSITION -= dieRoll;
+                            }
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("There will be no play. Player stays at same position");
+                        }
+                        break;
+
+                }
+                Console.WriteLine("CURRENTPOSITION is " + CURRENTPOSITION);
+
+
+            }
         }
     }
 }
